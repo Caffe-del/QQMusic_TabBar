560f2903fe6b24f63972771703ca6a3efec5893e
Darren <quandahui@gmail.com>
2020-08-23 15:00:48 +0800

    Initial Commit

---
+++ BottomBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BottomBar
+//
+//  Created by 全达晖 on 2020/8/23.
+//  Copyright © 2020 全达晖. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
